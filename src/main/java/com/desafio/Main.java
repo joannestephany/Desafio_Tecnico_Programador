c53f318c547b930f095345d88d6256b998503ef1
Joanne PC casa <joannestephany@gmail.com>
2025-12-03 17:03:52 -0300

    Add:: inicializa Maven e estrutura base

--- src/main/java/com/desafio/Main.java
+++ src/main/java/com/desafio/Main.java
@@ -1,4 +1,6 @@
 package com.desafio;
+import com.desafio.controller.VeiculoController;
+import com.desafio.model.Carro;
 import io.javalin.Javalin;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,6 +15,12 @@ public class Main {
             });
         }).start(7001);
 
-//        app.get("/veiculos", veiculoController::getAll);
+        VeiculoController veiculoController = new VeiculoController();
+
+        app.get("/veiculos", veiculoController::getAll);
+        app.get("/veiculos/{id}", veiculoController::getById);
+        app.post("/veiculos", veiculoController::post);
+        app.put("/veiculos/{id}", veiculoController::put);
+        app.delete("/veiculos/{id}", veiculoController::delete);
     }
 }
